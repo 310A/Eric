9cd9974df84f0a22ff1cef047226d3fa8935973d
847151766 <847151766@qq.com>
2017-10-14 15:00:04 +0800

    add function practice

---
+++ Eric/YEQI/Function/Function/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Function
+{
+    class Program
+    {    
+        static void Main(string[] args)
+        {
+            string[] num = {"aa","cc","cd"};
+            
+           Console.WriteLine(sentence(num,"|"));
+           Console.ReadKey();
+        }
+
+        public static string sentence(string[] str, string tag)
+        {
+            string temp =str[0] ;
+        for(int i=1;i<str.Length-1;i++)
+            {
+            temp=temp+tag+str[i];
+            
+            }
+        return temp+tag+str[str.Length-1]+tag;
+        }
+    }
+}
