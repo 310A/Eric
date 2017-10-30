28e50aafd4b286cbb7d17d1574f354937aa3de8f
StormSpirit22 <icestormspirit@gmail.com>
2017-10-30 09:38:34 +0800

    merge files

---
+++ Eric/ZHOUDIU/PythonSpiderNotes/ImageSpider/imageSpider.py
@@ -0,0 +1,7 @@
+import re
+import requests
+
+url = 'http://image.baidu.com/search/flip?tn=baiduimage&ipn=r&ct=201326592&cl=2&lm=-1&st=-1&fm=result&fr=&sf=1&fmq=1460997499750_R&pv=&ic=0&nc=1&z=&se=1&showtab=0&fb=0&width=&height=&face=0&istype=2&ie=utf-8&word=%E5%B0%8F%E9%BB%84%E4%BA%BA'
+
+html = requests.get(url).text
+pic_url = re.findall()
